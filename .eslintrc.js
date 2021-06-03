0cf5d333adcd54ca1e66b36761c2a2780be85ba9
CorentinVaugon <vaugon.corentin@gmail.com>
2021-06-03 21:33:03 +0200

    init(eslint): install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": "plugin:vue/essential",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "parser": "@typescript-eslint/parser",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": ["error", 2]
+  }
+};
