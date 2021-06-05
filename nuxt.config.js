bc4775f10233a8be56d249e6e40e6f91d40021fd
CorentinVaugon <vaugon.corentin@gmail.com>
2021-06-05 11:33:28 +0200

    feat(nuxt-composition-api): add nuxt/composition api on project

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,8 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    // https://composition-api.nuxtjs.org/
+    '@nuxtjs/composition-api/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
